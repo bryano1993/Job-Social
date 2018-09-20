99cb847b2d0a07a0cf743d02914edfad947cada7
Bryan <bryano1993@gmail.com>
2018-09-20 14:52:46 -0700

    creating-the-user-model:complete

---
+++ models/User.js
@@ -0,0 +1,30 @@
+//be singular and start with a capital
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//creating the user model
+//create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
