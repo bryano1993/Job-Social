5ed3caef867b2d9aab1c8222911d66f6d356f621
Bryan <bryano1993@gmail.com>
2018-09-20 10:29:13 -0700

    routes-files-with-express-router:complete

---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+//deals with authentication
+const express = require("express");
+const router = express.Router();
+
+//@route GET api/users/test
+//@desc Tests users route
+//@access Public
+
+router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+module.exports = router;
