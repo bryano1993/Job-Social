7a815a46c00fd8d16c6f3b4d5ac7a010b545f8b4
Bryan <bryano1993@gmail.com>
2018-09-25 09:40:57 -0700

    33-react-router-setup:complete

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,14 @@
+//class based component
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,14 @@
+//class based component
+import React, { Component } from "react";
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    );
+  }
+}
+
+export default Register;
