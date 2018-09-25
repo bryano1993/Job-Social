a40e86c2afe15f10253180e19570ef8660bb789b
Bryan <bryano1993@gmail.com>
2018-09-25 13:10:11 -0700

    40-redux-store-chrome-extension-setup:complete

---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducer, combineReducers } from "redux";
+import authReducer from "./authReducer";
+
+export default combineReducers({
+  auth: authReducer
+});
+
+//combine reducers
