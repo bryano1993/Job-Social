8781c4379c1a1242cd7c36dd85a9adc1b3dcc574
Bryan <bryano1993@gmail.com>
2018-09-25 16:12:32 -0700

    44-login-form-functionality:complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Provider } from "react-redux"; //takes in store. takes in a reducer, preloaded state, enhancers(middleware(redux chrome extension))
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
+import { Provider } from "react-redux"; //takes in store. takes in a reducer, preloaded state, enhancers(middleware(redux chrome extension)
 import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
@@ -11,6 +15,27 @@ import Login from "./components/auth/Login";
 
 import "./App.css";
 
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+
+    // Redirect to login
+    window.location.href = "/login";
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -19,7 +44,7 @@ class App extends Component {
           <div className="App">
             <Navbar />
             <Route exact path="/" component={Landing} />
-            <div className="contain">
+            <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
             </div>
@@ -32,4 +57,5 @@ class App extends Component {
 }
 
 export default App;
+
 //main app component that will hold everything  else
