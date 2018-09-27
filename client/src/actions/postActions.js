554a57c28759be7610e375ec797d3ed8fb36cb0b
Bryan <bryano1993@gmail.com>
2018-09-27 13:41:28 -0700

    71-delete-like-and-unlike-post:complete

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS, GET_POST, POST_LOADING } from "./types";
+import {
+  ADD_POST,
+  GET_ERRORS,
+  CLEAR_ERRORS,
+  GET_POSTS,
+  GET_POST,
+  POST_LOADING,
+  DELETE_POST
+} from "./types";
 
-//Add Post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -20,8 +28,8 @@ export const addPost = postData => dispatch => {
     );
 };
 
-//Get Post
-export const getPost = () => dispatch => {
+// Get Posts
+export const getPosts = () => dispatch => {
   dispatch(setPostLoading());
   axios
     .get("/api/posts")
@@ -39,6 +47,68 @@ export const getPost = () => dispatch => {
     );
 };
 
+// Get Post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POST,
+        payload: null
+      })
+    );
+};
+
+// Delete Post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 //set loading state
 export const setPostLoading = () => {
   return {
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,5 +1,10 @@
 //for any new resources, create a  reducer
-import { ADD_POST, GET_POSTS, POST_LOADING } from "../actions/types";
+import {
+  ADD_POST,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST
+} from "../actions/types";
 
 const initialState = {
   posts: [],
@@ -24,6 +29,11 @@ export default function(state = initialState, action) {
       return {
         ...state,
         posts: [action.payload, ...state.posts]
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
       };
     default:
       return state;
